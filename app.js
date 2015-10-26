7fab950b1583454beeeccfdedc12f06a7a9fd159
Steffen Kühne <stekhn@gmail.com>
2015-10-26 14:58:34 +0100

    Add async callbacks and getCompanyName

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var request = require('request');
+var cheerio = require('cheerio');
 var url = require('url');
 var htmlparser = require('htmlparser');
 var fs = require('fs');
 var Twit = require('twit');
 
 // Config variables
-var rssUrl = 'http://ted.europa.eu/TED/rss/CustomRSSFeedGenerator/239154/de?fmt=xml';
-var intervalLength = 2000;
+var rssUrl = 'http://ted.europa.eu/TED/rss/CustomRSSFeedGenerator/239154/de';
 
 var twitter = new Twit({
 
-    consumer_key: process.env.CONSUMER_KEY || 'engp8PvvP9kMEb8MrE2qT8DPw',
-    consumer_secret: process.env.CONSUMER_SECRET || 'HbRprEITtD3K25Hn47P6qzzaoLuCSXURrnsHm9GfknK0cTQe5M',
-    access_token: process.env.ACCESS_TOKEN || '3577194683-jmBzibEJM9gEcy9QqSfpmlYzxehXtlzeb9xYUUm',
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET || 'FgsUeQxn1sg5Agx0jlxkpB4qb5BxWfYYnl78XcGAJ6mtT'
+    consumer_key: process.env.CONSUMER_KEY                  || 'engp8PvvP9kMEb8MrE2qT8DPw',
+    consumer_secret: process.env.CONSUMER_SECRET            || 'HbRprEITtD3K25Hn47P6qzzaoLuCSXURrnsHm9GfknK0cTQe5M',
+    access_token: process.env.ACCESS_TOKEN                  || '3577194683-jmBzibEJM9gEcy9QqSfpmlYzxehXtlzeb9xYUUm',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET    || 'FgsUeQxn1sg5Agx0jlxkpB4qb5BxWfYYnl78XcGAJ6mtT'
 });
 
 // Get date of latest posted article
@@ -23,86 +23,130 @@ var lastestPostedItemDate = getLatestPostedItemDate();
 var handler = new htmlparser.RssHandler();
 var parser = new htmlparser.Parser(handler);
 
+(function init() {
+
+    getRSSFeed(handleFeed);
+})();
+
 // function to sort of dates
 function compareDates(a, b) {
 
     var aDate = new Date(a.pubDate);
     var bDate = new Date(b.pubDate);
 
-    if (aDate < bDate) {
+    if (aDate < bDate) { return -1; }
+    if (aDate > bDate) { return 1; }
 
-        return -1;
-    }
-
-    if (aDate > bDate) {
-
-        return 1;
-    }
     return 0;
 }
 
-// get the date (uses flat file to be replaced with MongoDB)
+// get the date of the last run
 function getLatestPostedItemDate() {
 
     var dateString = fs.readFileSync('lastestPostedDate.txt').toString();
     return new Date(dateString);
 }
 
-// set the date (uses flat file to be replaced with MongoDB)
+// save the date of the last run
 function setLatestPostedItemDate(date) {
 
     lastestPostedItemDate = date;
-    // write to file
     fs.writeFile('lastestPostedDate.txt', lastestPostedItemDate);
     return true;
 }
 
 // post item to twitter
 function publishToTwitter(item) {
-    var tweet = item.title + ' ' + item.link;
-    console.log('publishing to twitter');
-    
-    twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
-		     if (err)
-			 console.log(err);
-		     console.log(data);
-		 });
 
+    var tweet = item.title + ' ' + item.link;
+    
+    twitter.post('statuses/update', { status: tweet }, function (err, data, response) {
+             
+        if (err) { console.log(err); }
+         
+        console.log(data);
+     });
 }
 
-// looping on the server (every second)
-setInterval(function() {
+function getCompanyName(item, callback) {
 
-    request({uri: rssUrl}, function(err, response, body){
+    item.company = '';
 
-        // Basic error check
-        if(err && response.statusCode !== 200){
-            
-            console.log('Request error.');
-        }
-        
-        parser.parseComplete(body);
-        var items = handler.dom.items;
-        var itemsToPublish = []; // Array
+    request(item.link, { jar: true }, function (err, res, body) {
 
-        for(var key in items){
+        if (!err && res.statusCode == 200) {
 
-            //console.log(prop + ': ' + items[prop].title + ' ' + items[prop].link + '\n');
-            var itemDate = new Date(items[key].pubDate);
-            if(itemDate > lastestPostedItemDate){
-                // add to a publish array here
-                itemsToPublish.push(items[key]);
+            var $ = cheerio.load(body);
+            var addressEl = $('.mlioccur > .timark:contains("Wirtschaftsteilnehmers")').next().children().html();
+
+            if (addressEl) {
+
+                item.company = addressEl.split('<br>')[0];
             }
-        }
-        // sort items to publish on pubDate
-        itemsToPublish.sort(compareDates);
 
-        for(var i in itemsToPublish){
+            callback(item);
 
-            console.log(itemsToPublish[i].pubDate + ' ' + itemsToPublish[i].title);
-            publishToTwitter(itemsToPublish[i]);
-            setLatestPostedItemDate(itemsToPublish[i].pubDate);
+        } else {
+
+            console.log(err.message);
         }
     });
-    console.log('\n');
-}, intervalLength);
+}
+
+function getRSSFeed(callback) {
+
+    var items;
+    var itemsToPublish = [];
+
+    request(rssUrl, function (err, res, body) {
+
+        if (!err && res.statusCode == 200){
+            
+            parser.parseComplete(body);
+
+            items = handler.dom.items;
+            itemsToPublish = [];
+
+            for (var key in items) {
+
+                var itemDate = new Date(items[key].pubDate);
+                
+                if (itemDate > lastestPostedItemDate) {
+
+                    itemsToPublish.push(items[key]);
+                }
+            }
+
+            callback(itemsToPublish.sort(compareDates));
+
+        } else {
+
+            console.log(err.message);
+        }
+    });
+}
+
+function handleFeed(items) {
+
+    for (var key in items) {
+
+        items[key].company = getCompanyName(items[key], handleItem);
+    }
+}
+
+function handleItem(item) {
+
+    console.log(item.company);
+}
+
+function handleTweet(data) {
+
+    console.log(itemsToPublish[i].pubDate + ' ' + itemsToPublish[i].title);
+    // publishToTwitter(itemsToPublish[i]);
+    // setLatestPostedItemDate(itemsToPublish[i].pubDate);
+}
+
+
+
+
+
